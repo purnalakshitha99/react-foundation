6960151d01271c1d2534e290568074dc6fd7bba6
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-04-04 17:24:51 +0530

    arraow function implemented

---
+++ T-03-Arrow Function/arrow_function.js
@@ -0,0 +1,14 @@
+function greet(name){
+    return "Hello " + name + '!';
+} 
+
+console.log(greet('purna'))
+
+
+const arrowGreet = (name)=>{
+    return "hello " + name + '! i am arrow function ';
+}
+
+
+
+console.log(arrowGreet('widura'))
