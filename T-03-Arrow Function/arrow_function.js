eafc2841f18ec4c426ea4295f7bcd25560fc7e30
purnalakshitha99 <purnalakshitha2@gmail.com>
2025-04-20 22:18:40 +0530

    arrow function and normal function commented

--- T-03-Arrow Function/arrow_function.js
+++ T-03-Arrow Function/arrow_function.js
@@ -1,10 +1,12 @@
+
+//normal function
 function greet(name){
     return "Hello " + name + '!';
 } 
 
 console.log(greet('purna'))
 
-
+//arrow function
 const arrowGreet = (name)=>{
     return "hello " + name + '! i am arrow function ';
 }
